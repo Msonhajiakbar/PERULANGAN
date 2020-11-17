6c024c3f2f0d97351e0b20b601044ca57da821e3
Muhammad Sonhaji Akbar <mson.akbar@gmail.com>
2020-11-17 21:46:46 +0700

    perulangan akbar

---
+++ src/perulangan/PERULANGAN.java
@@ -0,0 +1,19 @@
+package perulangan;
+public class PERULANGAN {
+    
+    public static void hitung(double p, double l){//parameter
+        double luas;
+        luas=p*l;//40
+        System.out.println(luas);
+        luas=p+l+p;//
+        System.out.println(luas);
+    }
+    
+    public static void main(String[] args) {
+        int i=10;
+        hitung(4,i);//argumen
+    }
+    
+    
+    
+}
